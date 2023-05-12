f7949f9a196393767f867a9914fdcc278990e3ee
good_news <marymag69@outlook.com>
2023-05-12 20:03:50 +0100

    - changed names of extra scripts.

--- Assets/Scripts/Aristophanes/APlayer.cs
+++ Assets/Scripts/Aristophanes/APlayer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class APlayer : MonoBehaviour
 {
     [SerializeField]
     private float movSpeed = 1;
--- Assets/Scripts/Aristophanes/APlayerAnimator.cs
+++ Assets/Scripts/Aristophanes/APlayerAnimator.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAnimator : MonoBehaviour
+public class APlayerAnimator : MonoBehaviour
 {
     private const string IS_WALKING = "IsWalking";
     [SerializeField]
     private Animator animator;
     [SerializeField]
-    private Player player;
+    private APlayer player;
     // Start is called before the first frame update
     void Start()
     {
